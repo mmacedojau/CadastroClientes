3fcbcb51608d369490676a243d60c157ca985b04
Matheus Macedo <matheusmacedojau@hotmail.com>
2023-05-17 20:32:16 -0300

    Adicionado Pastas do JS e CSS

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Meus Clientes</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+  </head>
+  <body>
+    <h1>Teste de Servidor</h1>
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
