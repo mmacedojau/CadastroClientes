e9d288712fb64d4fd9049350787ceb1d0e22a91e
Matheus Macedo <matheusmacedojau@hotmail.com>
2023-05-17 20:39:45 -0300

    Criação do Menu da página Inicial

--- index.php
+++ index.php
@@ -7,7 +7,29 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" >
   </head>
   <body>
-    <h1>Teste de Servidor</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Meus Clientes</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Início</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=novo">Novo Cliente</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=listar">Listar Clientes</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    
     <script src="js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
